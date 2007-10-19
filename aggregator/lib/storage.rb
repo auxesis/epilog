c2ebd393cf29ae501a1419c4cb381c34bcecc88c
Lindsay Holmwood <lindsay@holmwood.id.au>
2007-10-19 23:51:22 +1000

    code cleanups - yay! gotten rid of awkward getters/setters in favour of attr_accessor, pass in a simple 'data' object (dictionary) on commit instead of countless crazy keywords, other clarity fixes

--- aggregator/lib/storage.rb
+++ aggregator/lib/storage.rb
@@ -6,7 +6,6 @@ require 'fileutils'
 require 'ferret'
 
 #require File.join(File.dirname(__FILE__), '..', 'lib', 'rfc3164')
-#require File.join(File.dirname(__FILE__), '..', 'lib', 'db')
 require 'lib/rfc3164'
 
 include Ferret
@@ -15,21 +14,14 @@ class Entry < ActiveRecord::Base ; end
 
 module Epilog
 
+  # Handles databases through ActiveRecord, and indexing through Ferret
+
   class DBStorage
-    # Class for dealing with datastores. Handles databases
-    # through ActiveRecord, and indexes through Ferret
 
-    def initialize(environment)
-      # temporary hack for setting the database and index environment
-      @environment = environment
-    end
-
-    def get_database_config(config='database.yml')
-      @db_config = YAML::load(File.open(config))
-    end
+    attr_accessor :environment, :database_config
 
     def connect_to_database
-      ActiveRecord::Base.configurations = @db_config
+      ActiveRecord::Base.configurations = @database_config
       ActiveRecord::Base.establish_connection(ActiveRecord::Base.configurations[@environment])
     end
 
@@ -40,15 +32,13 @@ module Epilog
       @index = Index::Index.new(:path => path)
     end
 
-    def store(message, datetime, digest, filename)
+    def store(data)
+      digest = data[:digest] 
       if Entry.find(:first, :conditions => [ "digest = ?", digest]) then
         puts "Entry #{digest} already exists!".yellow
       else
         begin
-          entry = Entry.new("message" => message, 
-                            "datetime" => datetime, 
-                            "digest" => digest, 
-                            "filename" => filename) 
+          entry = Entry.new(data)
           entry.save
           return entry
         rescue SQLite3::BusyException
@@ -63,21 +53,20 @@ module Epilog
         @index << { :message => entry.message, 
                     :datetime => entry.datetime,
                     :id => entry.id,
-                    :digest => entry.digest 
-        } # the search in the rails app doesn't work without 
-          # passing in the digest. i'd like to know why!
+                    :digest => entry.digest } 
         puts entry.message
-        puts entry.datetime
     end
 
-    def commit(message, datetime, digest, filename, filestat)
+    def commit(data, filestat)
       # we pass in the mtime of the file in as the year.
       # there needs to be a better check for working out
       # what the year *actually* is.
-      year = filestat.mtime.year
-      time = Epilog::RFC3164.to_ruby_datetime(datetime, year)  
 
-      entry = store(message, time, digest, filename)
+      year = filestat.mtime.year
+      ruby_datetime = Epilog::RFC3164.to_ruby_datetime(data[:datetime], year)  
+      data[:datetime] = ruby_datetime
+
+      entry = store(data)
 
       index(entry) unless entry.blank?
 
